af8204463d046326aae9e5eadfd9f865b4fc7e61
hard <harleydav@mail.ru>
2018-01-03 13:41:59 +0300

    refactor

--- src/main/java/com/hard/models/User.java
+++ src/main/java/com/hard/models/User.java
@@ -19,12 +19,16 @@ public class User {
     @ManyToMany
     @JoinTable(
             name = "user_role",
-            joinColumns = @JoinColumn(
-                    name = "user_id"
-            ),
-            inverseJoinColumns = @JoinColumn(
-                    name = "role_id"
-            )
+            joinColumns = {
+                    @JoinColumn(
+                            name = "user_id"
+                    ),
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "role_id"
+                    )
+            }
     )
     private Set<Role> roles;
 
